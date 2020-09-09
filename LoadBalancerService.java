0e49d8d116dc01ddeace2e5fdd4ffb677c21cb2a
voidbean <gydforwin@outlook.com>
2020-09-09 15:29:32 +0800

    init

---
+++ LoadBalancerService.java
@@ -0,0 +1,10 @@
+package com.voidbean.service;
+
+import com.voidbean.model.MyRibbon
+
+public interface LoadBalancerService {
+
+	//Get a ribbon rule
+    MyRibbon getLoadBalancerByName(String name);
+
+}
